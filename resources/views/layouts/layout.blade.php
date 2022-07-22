a458a7c5cff742ec7fbe9fbb86bbffe76df61437
Bhopal Singh <bhopalsinghsis@gmail.com>
2022-07-22 16:36:34 -0400

    add logout on home page

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -54,16 +54,25 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a class="text-sm text-gray-700 dark:text-gray-500 underline" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>&nbsp;&nbsp;
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
+                        
                     @endauth
                 </div>
             @endif
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                  @yield('content')
